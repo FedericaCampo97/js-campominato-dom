afc740b95798154fd9ee841e63bda4e43794fa49
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-04 13:03:20 +0200

    init red blue cell

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,13 @@ const btn = document.getElementById('btn')
 btn.addEventListener("click", generateGrid);
 
 function generateGrid() {
+    //Genero 16 numeri random che non si ripetono
+    const arrayBombs = []
+    while (arrayBombs.length < 16) {
+        let number = Math.floor(Math.random() * 100) + 1;
+        if(arrayBombs.indexOf(number) === -1) arrayBombs.push(number);
+    }
+    console.log(arrayBombs)
     //Ricavo il container dove andranno le mie celle
     const container = document.getElementById('container')
     //Ciclo per 100 celle
@@ -13,22 +20,29 @@ function generateGrid() {
         //aggiungo la classe cell
         cellElement.className = 'cell';
         cellElement.append(i+1)
+
+        if (arrayBombs.indexOf(i) >= 0) {
+            //Aggiungo una funzione al click della cella
+            cellElement.addEventListener('click', function () {
+                //Mostro in console il numero cliccato
+                console.log('ho cliccato sulla cella numero:', i + 1);
+                //Cambio il background della cella
+                this.style.background = 'red'
+            })
+        }
+        else{
+            //Aggiungo una funzione al click della cella
+            cellElement.addEventListener('click', function () {
+                //Mostro in console il numero cliccato
+                console.log('ho cliccato sulla cella numero:', i + 1);
+                //Cambio il background della cella
+                this.style.background = 'aqua'
+            })
+        }
         //Appendo l'elemento al mio container
         container.append(cellElement)
-
-        //Aggiungo una funzione al click della cella
-        cellElement.addEventListener('click', function () {
-            //Mostro in console il numero cliccato
-            console.log('ho cliccato sulla cella numero:', i + 1);
-            //Cambio il background della cella
-            this.style.background = 'aqua'
-        })
+        
+        
     }
+        
 }
-//Genero 16 numeri random che non si ripetono
-const arrayBombs = []
-while (arrayBombs.length < 16) {
-    let number = Math.floor(Math.random() * 100) + 1;
-    if(arrayBombs.indexOf(number) === -1) arrayBombs.push(number);
-}
-console.log(arrayBombs)
